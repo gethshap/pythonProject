091e72fdeb36965023d54f377b68be62da61a6b9
hw87147 <hw87147@gmail.com>
2023-03-17 02:16:46 -0700

    \omg

--- main.py
+++ main.py
@@ -279,13 +279,15 @@ class Basic_block:
         self.ssa_lines=[]
 
 
-
+"""
 s = Digraph('struct',filename="control_flow_graph",node_attr={'shape':'record'})
 s.node('struct1', ' left| middle| right')
 s.node('struct2', ' one| two')
 s.node('struct3', r'hello\nworld |{ b |{c| d|e}| f}| g | h')
 s.edges([('struct1:f1', 'struct2:f0'), ('struct1:f2', 'struct3:here')])
 s.view()
+"""
+
 
 
 
